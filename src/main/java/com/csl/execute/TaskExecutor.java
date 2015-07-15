95b07cf075f919b130526e23449e0e27b66b548f
chensongle <chensongle@huoyunren.com>
2015-07-15 15:36:35 +0800

    任务框架修改

--- src/main/java/com/csl/execute/TaskExecutor.java
+++ src/main/java/com/csl/execute/TaskExecutor.java
@@ -1,16 +1,34 @@
 package com.csl.execute;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class TaskExecutor {
-    private static ScheduledExecutorService  pool = Executors.newScheduledThreadPool(8);
+    private static ScheduledThreadPoolExecutor  executor = new ScheduledThreadPoolExecutor(8){
+        protected void beforeExecute(Thread t, Runnable r) {
+            System.out.println("目前有任务---" +getTaskSize());
+            System.out.println("现在执行任务---");
+        };
+        protected void afterExecute(Runnable r, Throwable t) {
+            System.out.println("任务结束---");
+            System.out.println("目前有任务---" +getTaskSize());
+        };
+    };
     public static void addTask(Runnable task, int time, TimeUnit unit) {
-        pool.schedule(task, time, unit);
+        executor.schedule(task, time, unit);
+    }
+    public static void addScheduledTask(Runnable task ,int time, TimeUnit unit){
+        executor.scheduleWithFixedDelay(task, 0, time, unit);
     }
 
     public static void addTaskNow(Runnable task) {
         addTask(task, 1, TimeUnit.SECONDS);
+    }
+    public static int getTaskSize(){
+        return executor.getQueue().size();
+    }
+    public static BlockingQueue<Runnable> getTasks(){
+        return executor.getQueue();
     }
 }

2c2c41e4b04b51aa4f8de350b5e73fbe300afd1c
ditmak <chslzj@163.com>
2015-07-25 19:28:53 +0800

    牧场饲养

--- src/main/java/com/csl/qq/card/task/SimplePastureTasker.java
+++ src/main/java/com/csl/qq/card/task/SimplePastureTasker.java
@@ -3,50 +3,123 @@ package com.csl.qq.card.task;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.dom4j.Element;
 
 import com.csl.execute.TaskExecutor;
 import com.csl.util.net.HTTPUtil;
 
-public class SimplePastureTasker extends BaseTask{
+public class SimplePastureTasker extends BaseTask {
     private static String pastureURL = "http://mcapp.z.qq.com/mc/cgi-bin/wap_pasture_index?sid=";
     private static List<String> keyList = new ArrayList<String>();
-    static{
+    private static Pattern canPattern  = Pattern.compile("[(](\\d+)[/](\\d+).*?[(](\\d+)[/](\\d+)");
+    private static  Pattern woPattern = Pattern.compile("窝\\).*?href=\"(.*?)\">饲养",Pattern.DOTALL);
+    private static  Pattern pengPattern = Pattern.compile("棚\\).*?href=\"(.*?)\">饲养",Pattern.DOTALL);
+    static {
         keyList.add("生产");
         keyList.add("收获");
         keyList.add("清扫");
+        keyList.add("饲养");
     }
+
     public SimplePastureTasker(String sid) {
-        this.sid= sid;
+        this.sid = sid;
     }
+
     @Override
     public void doSomeThing() {
-        List<Element> aTagListByURL = HTTPUtil.getATagListByURL(pastureURL+sid);
+        List<Element> aTagListByURL = HTTPUtil.getATagListByURL(pastureURL
+                + sid);
         doMainPage(aTagListByURL);
     }
-    private void doMainPage(List<Element> eles){
+
+    private void doMainPage(List<Element> eles) {
         boolean flag = false;
+        boolean bflag = false;
         for (Element aTag : eles) {
             String text = aTag.getTextTrim();
             String href = aTag.attributeValue("href");
-            if(keyList.contains(text)){
-                //static.... 跳过
-                if(href.contains("static"))
+            if (keyList.contains(text)) {
+                // static.... 跳过
+                if (href.contains("static"))
                     continue;
-                //生产的话，20秒之后添加一个任务
-                if(text.equals("生产")){
-                   flag = true;
+                // 生产的话，20秒之后添加一个任务
+                if (text.equals("生产")) {
+                    flag = true;
                 }
-                //收获 ..饲养 如果满了会有饲养吗？
-                if(href.startsWith("."))
-                    href =href.replace(".","http://mcapp.z.qq.com/mc/cgi-bin" );
+                if (text.equals("饲养")) {
+                    if (href.startsWith("http://mcapp.z.qq.com/mc/cgi-bin/wap_pasture_bag_list")) {
+                         toHaveAnimal(href);
+                    }
+                }
+                if(text.equals("收获")){
+                    bflag= true;
+                }
+                // 收获 ..饲养 如果满了会有饲养吗？
+                if (href.startsWith("."))
+                    href = href
+                            .replace(".", "http://mcapp.z.qq.com/mc/cgi-bin");
                 HTTPUtil.getURLContent(href);
-                            }
+            }
         }
-       if(flag){
-           TaskExecutor.addTask(this,20, TimeUnit.SECONDS);
-       }
-        
+        if (flag) {
+            TaskExecutor.addTask(this, 20, TimeUnit.SECONDS);
+        }
+        if(bflag){
+            TaskExecutor.addTask(this, 3, TimeUnit.SECONDS);
+        }
+
+    }
+
+    private void toHaveAnimal(String href) {
+        String context = HTTPUtil.getURLContent(href);
+        Matcher canMather = canPattern.matcher(context);
+        canMather.find();
+        int woNum  = getNum(canMather.group(1),canMather.group(2));
+        int pengNum = getNum(canMather.group(3),canMather.group(4));
+        if(woNum>0)
+        {
+            Matcher matcher = woPattern.matcher(context);
+            if(matcher.find()){
+                String url  = matcher.group(1);
+                Pattern pattern  = Pattern.compile("num=(\\d+)");
+                 matcher = pattern.matcher(url);
+                 matcher.find();
+                 int num = Integer.parseInt(matcher.group(1));
+                 num = woNum>=num?num:woNum;
+                 url=matcher.replaceFirst("num="+num);
+                 url = url.replace("_info", "");
+                 url = url.replaceAll("&amp;", "&");
+                 if(url.startsWith("."));
+                     url = url.replace(".", "http://mcapp.z.qq.com/mc/cgi-bin");
+                 HTTPUtil.getURLContent(url);
+                 toHaveAnimal(href);
+            }
+        }else if(pengNum>0){
+            Matcher matcher = pengPattern.matcher(context);
+            if(matcher.find()){
+                String url  = matcher.group(1);
+                Pattern pattern  = Pattern.compile("num=(\\d+)");
+                 matcher = pattern.matcher(url);
+                 matcher.find();
+                 int num = Integer.parseInt(matcher.group(1));
+                 num = pengNum>=num?num:pengNum;
+                 url=matcher.replaceFirst("num="+num);
+                 url = url.replace("_info", "");
+                 url = url.replaceAll("&amp;", "&");
+                 if(url.startsWith("."));
+                     url = url.replace(".", "http://mcapp.z.qq.com/mc/cgi-bin");
+                 HTTPUtil.getURLContent(url);
+                 toHaveAnimal(href);
+            }
+        }
+
+    }
+    private int getNum(String num,String num2){
+        int total = Integer.parseInt(num2);
+        int now = Integer.parseInt(num);
+        return total-now;
     }
 }
--- src/test/java/com/csl/qq/card/dao/impl/test/OperatorDAOImplTest.java
+++ src/test/java/com/csl/qq/card/dao/impl/test/OperatorDAOImplTest.java
@@ -20,23 +20,32 @@ public class OperatorDAOImplTest {
         }
         @Test
         public void test4(){
+            Pattern canPattern = Pattern.compile("窝\\).*?href=\"(.*?)\">饲养",Pattern.DOTALL);
            Pattern boxsize = Pattern.compile("\\[(\\d+)\\][^0-9]*?(\\d+)?[^0-9]*?已有(\\d+)张.*?(http.*?)\">",Pattern.DOTALL);
-           String url = "http://mfkp.qzapp.z.qq.com/qshow/cgi-bin/wl_card_strategy?sid=AS9AWzaHQVxDUuwj9QRq0beE";
+           String url = "http://mcapp.z.qq.com/mc/cgi-bin/wap_pasture_bag_list?sid=AS9AWzaHQVxDUuwj9QRq0beE";
            String content = HTTPUtil.getURLContent(url);
-           Matcher matcher = boxsize.matcher(content);
+           Matcher matcher = canPattern.matcher(content);
            int count =0;
            while(matcher.find()){
               System.out.println("------------"+(++count)+"begin----------");
                System.out.println(matcher.group(1));
-               System.out.println(matcher.group(2));
-               System.out.println(matcher.group(3));
-               System.out.println(matcher.group(4));
                System.out.println("------------"+count+"end----------");
            }
+
         }
 	@Test
 	public void test1(){
-		System.out.println(Integer.highestOneBit(101));
+	    int total = 5;
+	    String url = "./wap_pasture_plant_info?sid=AS9AWzaHQVxDUuwj9QRq0beE&amp;g_ut=1&amp;cid=1279&amp;num=90&amp;name=骑行猫&amp;page=1";
+	    Pattern pattern  = Pattern.compile("num=(\\d+)");
+           Matcher  matcher = pattern.matcher(url);
+            matcher.find();
+            int num = Integer.parseInt(matcher.group(1));
+            num = total>=num?num:total;
+            url=matcher.replaceFirst("num="+num);
+	}
+	public void test2(){
+	    
 	}
 
 }
